b63239e64cf25e8a2cf1dfb491c6a7d635a407e1
andreasweiner@gmail.com <andreasweiner@gmail.com@489398c6-052e-fef8-193d-45aa9c77b1f9>
2012-10-06 13:14:32 +0000

    Initial solution with PhoneRental models.

---
+++ trunk/PhoneRental/PhoneRental/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PhoneRental.MvcApplication" Language="C#" %>
---
+++ trunk/PhoneRental/PhoneRental/Models/PhoneRentalModels.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace PhoneRental.Models
+{
+    public class PhoneRentalContext : DbContext
+    {
+        public PhoneRentalContext()
+            : base("DefaultConnection")
+        {
+        }
+
+        public DbSet<UserProfile> UserProfiles { get; set; }
+        public DbSet<DeviceType> DeviceTypes { get; set; }
+        public DbSet<Device> Devices { get; set; }
+        public DbSet<Borrow> Borrows { get; set; }
+        public DbSet<PreBorrow> PreBorrows { get; set; }
+    }
+
+    public class DeviceType
+    {
+        public int Id { get; set; }
+        public string Brand { get; set; }
+        public string Type { get; set; }
+        public string AaitIdPattern { get; set; }
+        public string ImageUrl { get; set; }
+        public virtual ICollection<Device> Devices { get; set; }
+    }
+
+    public class Device
+    {
+        public int Id { get; set; }
+        public int DeviceTypeId { get; set; }
+        public virtual DeviceType DeviceType { get; set; }
+        public string Imei { get; set; }
+        public int AaitIdNumber { get; set; }
+    }
+
+    public class Borrow
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public virtual UserProfile User { get; set; }
+        public int DeviceId { get; set; }
+        public virtual Device Device { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public DateTime Deadline { get; set; }
+    }
+
+    public class PreBorrow
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public virtual UserProfile User { get; set; }
+        public int DeviceTypeId { get; set; }
+        public virtual DeviceType DeviceType { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
