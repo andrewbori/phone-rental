2ce4951f0e18cf3f23bee4c23a5657574bdeebd1
boripeter@gmail.com <boripeter@gmail.com@489398c6-052e-fef8-193d-45aa9c77b1f9>
2012-11-11 11:01:00 +0000

    MyBorrowsController added

---
+++ trunk/PhoneRental/PhoneRental/Controllers/MyBorrowsController.cs
@@ -0,0 +1,47 @@
+﻿using PhoneRental.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PhoneRental.Controllers
+{
+    [Authorize(Roles = "Customer")]
+    public class MyBorrowsController : Controller
+    {
+        private PhoneRentalContext db = new PhoneRentalContext();
+
+        //
+        // GET: /MyBorrows/
+
+        public ActionResult Index()
+        {
+            var userId = db.UserProfiles.Single(
+                        p => p.UserName == HttpContext.User.Identity.Name).UserId;
+
+            var borrows = db.Borrows.Where(b => b.UserId == userId).ToList();
+            if (borrows.Count == 0)
+            {
+                ViewBag.HasBorrowed = false;
+            }
+            else
+            {
+                ViewBag.HasBorrowed = true;
+                ViewBag.Borrows = borrows;
+            }
+            var preBorrows = db.PreBorrows.Where(pb => pb.UserId == userId).ToList();
+            if (preBorrows.Count == 0)
+            {
+                ViewBag.HasPreBorrowed = false;
+            }
+            else
+            {
+                ViewBag.HasPreBorrowed = true;
+                ViewBag.PreBorrows = preBorrows;
+            }
+            return View();
+        }
+
+    }
+}
